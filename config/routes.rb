f782a5f0fad2876dd03dc6c2ae3e59e94ca9db9d
Steeve Postemus <spostemus@gmail.com>
2016-07-19 13:44:39 -0700

    added user profile and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
+
+  resources :users, only: [:show]
 
   root 'welcome#index'
 
   get 'about' => 'welcome#about'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
